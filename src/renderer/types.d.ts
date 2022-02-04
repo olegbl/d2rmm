dc622e29133f82582a629ef6a42700c9a7a53cad
Oleg Lokhvitsky <olegbl@gmail.com>
2022-02-04 09:03:23 -0800

    Document ModAPI a bit

--- src/renderer/types.d.ts
+++ src/renderer/types.d.ts
@@ -22,12 +22,59 @@ declare global {
   type JSONData = { [key: string]: JSONDataValues | JSONData };
 
   type ModAPI = {
-    copyFile: (src: string, dst: string) => void;
-    error: (message: string | Error) => void;
+    // reads a JSON D2R file (ignoring comments, whitespace, and various mistakes in JSON formatting that D2R makes)
+    // the file is the result of previously installed mods operating on it
+    // or is ripped directly from D2R game files if no preceding mod has needed it yet
+    // e.g.:
+    // const profileHD = D2RMM.readJson('global\\ui\\layouts\\_profilehd.json');
+    // profileHD.FontColorRed.r; // 252
     readJson: (filePath: string) => JSONData;
-    readTsv: (filePath: string) => TSVData;
+
+    // writes a JSON D2R file to the data directory
+    // e.g.:
+    // // change red colored text to bright green!
+    // const profileHD = D2RMM.readJson('global\\ui\\layouts\\_profilehd.json');
+    // profileHD.FontColorRed = {r: 0, b: 0, g: 255, a: 255};
+    // D2RMM.writeJson('global\\ui\\layouts\\_profilehd.json', profileHD);
     writeJson: (filePath: string, data: JSONData) => void;
+
+    // reads a TSV D2R file and parses it in JSON format where you can access columns by name instead of index
+    // the file is the result of previously installed mods operating on it
+    // or is ripped directly from D2R game files if no preceding mod has needed it yet
+    // e.g.:
+    // const treasureclassex = D2RMM.readTsv('global\\excel\\treasureclassex.txt');
+    // console.log('There are ' + treasureclassex.rows.length + ' treasure classes!');
+    // console.log('Each treasure class has ' + treasureclassex.headers.length + ' properties!');
+    readTsv: (filePath: string) => TSVData;
+
+    // writes a TSV D2R file to the data directory
+    // e.g.:
+    // const treasureclassex = D2RMM.readTsv('global\\excel\\treasureclassex.txt');
+    // treasureclassex.rows.forEach(row => {
+    //   // D2R TSV files sometimes have blank rows
+    //   if (row['Treasure Class'] !== '') {
+    //     row.NoDrop = 1;
+    //   }
+    // });
+    // D2RMM.writeTsv('global\\excel\\treasureclassex.txt', treasureclassex);
     writeTsv: (filePath: string, data: TSVData) => void;
+
+    // copies a file or directory from the mod directory to the data directory
+    // you should use this for non-mergeable assets like sprites
+    // avoid using it for TSV and JSON data (but I'm not the police, you do you)
+    // e.g.:
+    // D2RMM.copyFile(
+    //   'hd', // <mod folder>\hd
+    //   'hd', // <diablo 2 folder>\mods\D2RMM\D2RMM.mpq\data\hd
+    //   true // overwrite any conflicts
+    // );
+    copyFile: (src: string, dst: string, overwrite?: boolean) => void;
+
+    // shows an error message to the user (you probably don't need to do this manually, just throw the error)
+    // e.g.:
+    // D2RMM.erorr('Something went wrong!');
+    // D2RMM.error(new Error('Something went wrong!'));
+    error: (message: string | Error) => void;
   };
 
   type Mod = {

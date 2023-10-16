e6346531c57df6189a208940082968bdfa22d6ed
Oleg Lokhvitsky <olegbl@gmail.com>
2023-10-15 17:53:52 -0700

    Add option for global preferences flag, and default to local user data if it exists

--- src/main/preferences.ts
+++ src/main/preferences.ts
@@ -1,5 +1,3 @@
-/* eslint-disable import/prefer-default-export */
-
 import { app } from 'electron';
 import { existsSync } from 'fs';
 import path from 'path';
@@ -9,18 +7,41 @@ const APPLICATION_DIRECTORY = path.dirname(app.getPath('exe'));
 const GLOBAL_USER_DATA_DIRECTORY = app.getPath('userData');
 const LOCAL_USER_DATA_DIRECTORY = APPLICATION_DIRECTORY;
 
-export function initPreferences(): void {
-  // if the flag file doesn't exist, and there is data in %AppData%
-  // then we will continue to store userData in %AppData% as not to
-  // break existing configurations during an update
-  if (
-    !existsSync(path.join(APPLICATION_DIRECTORY, 'ENABLE_LOCAL_PREFERENCES')) &&
-    existsSync(path.join(GLOBAL_USER_DATA_DIRECTORY, 'Local Storage'))
-  ) {
-    console.log('Using global preferences');
-    return;
+const GLOBAL_USER_DATA_FLAG = 'ENABLE_GLOBAL_PREFERENCES';
+const LOCAL_USER_DATA_FLAG = 'ENABLE_LOCAL_PREFERENCES';
+
+const STORAGE_DIRECTORY = 'Local Storage';
+
+export function getIsUsingLocalUserDataDirectory(): boolean {
+  // explicit local preferences flag
+  if (existsSync(path.join(APPLICATION_DIRECTORY, LOCAL_USER_DATA_FLAG))) {
+    return true;
   }
 
-  console.log('Using local preferences');
-  app.setPath('userData', LOCAL_USER_DATA_DIRECTORY);
+  // explicit global preferences flag
+  if (existsSync(path.join(APPLICATION_DIRECTORY, GLOBAL_USER_DATA_FLAG))) {
+    return false;
+  }
+
+  // existing preferences in local directory
+  if (existsSync(path.join(LOCAL_USER_DATA_DIRECTORY, STORAGE_DIRECTORY))) {
+    return true;
+  }
+
+  // existing preferences in global directory
+  if (existsSync(path.join(GLOBAL_USER_DATA_DIRECTORY, STORAGE_DIRECTORY))) {
+    return false;
+  }
+
+  // default (local)
+  return true;
+}
+
+export function initPreferences(): void {
+  if (getIsUsingLocalUserDataDirectory()) {
+    console.log('Using local preferences');
+    app.setPath('userData', LOCAL_USER_DATA_DIRECTORY);
+  } else {
+    console.log('Using global preferences');
+  }
 }

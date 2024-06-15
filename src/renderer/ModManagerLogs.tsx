4604bccb802a77d7081412b29c16a89850e1df57
Oleg Lokhvitsky <olegbl@gmail.com>
2024-06-14 21:48:11 -0700

    Improve log viewer (better perf through virtualization, preview drawer, search)

--- src/renderer/ModManagerLogs.tsx
+++ src/renderer/ModManagerLogs.tsx
@@ -1,25 +1,35 @@
 import {
+  AppBar,
   Box,
   Button,
   Divider,
+  Drawer,
+  IconButton,
   InputAdornment,
-  List,
-  ListItem,
+  ListItemButton,
   ListItemIcon,
   ListItemText,
   Menu,
   MenuItem,
   Select,
   SelectChangeEvent,
+  TextField,
+  Toolbar,
+  Typography,
 } from '@mui/material';
-import ErrorIcon from '@mui/icons-material/Error';
-import WarningIcon from '@mui/icons-material/Warning';
-import InfoIcon from '@mui/icons-material/Info';
-import PendingIcon from '@mui/icons-material/Pending';
+import CloseIcon from '@mui/icons-material/Close';
+import ErrorOutlineIcon from '@mui/icons-material/ErrorOutlined';
+import ContentCopyOutlinedIcon from '@mui/icons-material/ContentCopyOutlined';
+import ReportProblemOutlinedIcon from '@mui/icons-material/ReportProblemOutlined';
+import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
+import PendingOutlinedIcon from '@mui/icons-material/PendingOutlined';
 import DownloadIcon from '@mui/icons-material/Download';
-import { MouseEvent, useCallback, useState } from 'react';
+import { MouseEvent, useCallback, useMemo, useState } from 'react';
 import FilterAltIcon from '@mui/icons-material/FilterAltOutlined';
 import { useLogLevels, useLogs } from './Logs';
+import { FixedSizeList, ListChildComponentProps } from 'react-window';
+import ReactVirtualizedAutoSizer from 'react-virtualized-auto-sizer';
+import SearchIcon from '@mui/icons-material/Search';
 
 function prettyPrintData(data: unknown): string {
   if (Array.isArray(data)) {
@@ -40,6 +50,8 @@ type Props = Record<string, never>;
 export default function ModManagerSettings(_props: Props): JSX.Element {
   const logs = useLogs();
   const [levels, setLevels] = useLogLevels();
+  const [filter, setFilter] = useState('');
+  const [selectedRowIndex, setSelectedRowIndex] = useState<number>(-1);
   const [exportAnchorEl, setExportAnchorEl] =
     useState<HTMLButtonElement | null>(null);
 
@@ -81,48 +93,84 @@ export default function ModManagerSettings(_props: Props): JSX.Element {
     onCloseExportMenu();
   }, [logs, onCloseExportMenu]);
 
+  const filteredLogs = useMemo(
+    () =>
+      logs
+        .filter((log) => levels.includes(log.level))
+        .map((log) => ({
+          ...log,
+          text: log.data.map(prettyPrintData).join(' '),
+        }))
+        .filter(
+          (log) =>
+            filter === '' ||
+            log.text.toLowerCase().includes(filter.toLowerCase())
+        ),
+    [logs, levels, filter]
+  );
+
+  const renderRow = useCallback(
+    ({ style, index }: ListChildComponentProps) => {
+      const log = filteredLogs[index];
+      return (
+        <ListItemButton
+          divider={index < filteredLogs.length - 1}
+          key={log.id}
+          onClick={() => setSelectedRowIndex(index)}
+          style={style}
+          sx={{ flex: 1, userSelect: 'none' }}
+        >
+          <ListItemIcon>
+            {log.level === 'error' && <ErrorOutlineIcon color="error" />}
+            {log.level === 'warn' && (
+              <ReportProblemOutlinedIcon color="warning" />
+            )}
+            {log.level === 'log' && <InfoOutlinedIcon color="primary" />}
+            {log.level === 'debug' && <PendingOutlinedIcon color="disabled" />}
+          </ListItemIcon>
+          <ListItemText
+            primary={log.text.slice(0, 512)}
+            primaryTypographyProps={{
+              sx: {
+                // clamp messages to a maximum number of lines to avoid some log messages
+                // with a lot of data in them from taking up too much space
+                display: '-webkit-box',
+                overflow: 'hidden',
+                WebkitLineClamp: 2,
+                WebkitBoxOrient: 'vertical',
+              },
+            }}
+            secondary={new Date(log.timestamp).toLocaleTimeString()}
+          />
+        </ListItemButton>
+      );
+    },
+    [filteredLogs]
+  );
+
+  const getItemKey = useCallback(
+    (index: number) => filteredLogs[index].id,
+    [filteredLogs]
+  );
+
   return (
     <>
-      <List
-        sx={{ width: '100%', flex: 1, overflow: 'auto' }}
-        disablePadding={true}
-        dense={true}
-      >
-        {logs
-          .filter((log) => levels.includes(log.level))
-          .map((log) => (
-            <ListItem key={log.id}>
-              <ListItemIcon>
-                {log.level === 'error' && <ErrorIcon color="error" />}
-                {log.level === 'warn' && <WarningIcon color="warning" />}
-                {log.level === 'log' && <InfoIcon color="primary" />}
-                {log.level === 'debug' && <PendingIcon color="disabled" />}
-              </ListItemIcon>
-              <ListItemText
-                primary={log.data.map(prettyPrintData).join(' ').slice(0, 512)}
-                primaryTypographyProps={{
-                  sx: {
-                    // clamp messages to a maximum number of lines to avoid some log messages
-                    // with a lot of data in them from taking up too much space
-                    display: '-webkit-box',
-                    overflow: 'hidden',
-                    WebkitLineClamp: 3,
-                    WebkitBoxOrient: 'vertical',
-                  },
-                }}
-                secondary={new Date(log.timestamp).toLocaleTimeString()}
-              />
-            </ListItem>
-          ))
-          .reduce(
-            (items: JSX.Element[], item: JSX.Element) => [
-              ...items,
-              <Divider key={`${item.key}-divider`} />,
-              item,
-            ],
-            []
+      <Box sx={{ width: '100%', flex: 1, bgcolor: 'background.paper' }}>
+        <ReactVirtualizedAutoSizer>
+          {({ height, width }) => (
+            <FixedSizeList
+              width={width}
+              height={height}
+              itemKey={getItemKey}
+              itemCount={filteredLogs.length}
+              overscanCount={5}
+              itemSize={80}
+            >
+              {renderRow}
+            </FixedSizeList>
           )}
-      </List>
+        </ReactVirtualizedAutoSizer>
+      </Box>
       <Divider />
       <Box
         sx={{
@@ -148,6 +196,34 @@ export default function ModManagerSettings(_props: Props): JSX.Element {
           <MenuItem value="log">Info</MenuItem>
           <MenuItem value="debug">Debug</MenuItem>
         </Select>
+        <TextField
+          size="small"
+          variant="outlined"
+          hiddenLabel={true}
+          placeholder="Search..."
+          value={filter}
+          onChange={(event) => setFilter(event.target.value)}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <SearchIcon />
+              </InputAdornment>
+            ),
+          }}
+          sx={{
+            flex: '0 1 auto',
+            display: 'flex',
+            flexDirection: 'row',
+            '& .MuiInputBase-root': {
+              width: '150px',
+              transition: 'width ease-in-out 0.35s 0s',
+              '&.Mui-focused': {
+                width: '400px',
+              },
+            },
+            marginLeft: 1,
+          }}
+        />
         <Box sx={{ flex: 1 }} />
         <Button
           variant="outlined"
@@ -164,6 +240,63 @@ export default function ModManagerSettings(_props: Props): JSX.Element {
           <MenuItem onClick={onCopy}>Copy to clipboard</MenuItem>
         </Menu>
       </Box>
+      <Drawer
+        anchor="bottom"
+        open={selectedRowIndex !== -1}
+        onClose={() => setSelectedRowIndex(-1)}
+      >
+        {selectedRowIndex === -1 ? null : (
+          <>
+            <AppBar position="static">
+              <Toolbar>
+                <Typography variant="h6" component="div">
+                  {filteredLogs[selectedRowIndex].level === 'error'
+                    ? 'Error'
+                    : filteredLogs[selectedRowIndex].level === 'warn'
+                    ? 'Warning'
+                    : filteredLogs[selectedRowIndex].level === 'log'
+                    ? 'Info'
+                    : filteredLogs[selectedRowIndex].level === 'debug'
+                    ? 'Debug'
+                    : ''}
+                </Typography>
+                <Box sx={{ flex: 1 }} />
+                <IconButton
+                  size="large"
+                  edge="start"
+                  color="inherit"
+                  onClick={() =>
+                    navigator.clipboard.writeText(
+                      filteredLogs[selectedRowIndex].text
+                    )
+                  }
+                  sx={{ marginRight: 2 }}
+                >
+                  <ContentCopyOutlinedIcon />
+                </IconButton>
+                <IconButton
+                  size="large"
+                  edge="start"
+                  color="inherit"
+                  onClick={() => setSelectedRowIndex(-1)}
+                >
+                  <CloseIcon />
+                </IconButton>
+              </Toolbar>
+            </AppBar>
+            <Box
+              sx={{
+                padding: 2,
+                flex: 1,
+                overflow: 'auto',
+                wordBreak: 'break-all',
+              }}
+            >
+              {filteredLogs[selectedRowIndex].text}
+            </Box>
+          </>
+        )}
+      </Drawer>
     </>
   );
 }

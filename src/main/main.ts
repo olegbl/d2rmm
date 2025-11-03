c7ce7784e8420f776edda867e799deb629356fc1
Oleg Lokhvitsky <olegbl@gmail.com>
2025-11-02 20:55:22 -0800

    Remove some boilerplate electron comments.

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,13 +1,3 @@
-/* eslint global-require: off, no-console: off, promise/always-return: off */
-
-/**
- * This module executes inside of electron's main process. You can start
- * electron renderer process from here and communicate with the other processes
- * through IPC.
- *
- * When running `npm run build` or `npm run build:main`, this file is compiled to
- * `./src/main.js` using webpack. This gives us some performance wins.
- */
 import 'core-js/stable';
 import { app, BrowserWindow, ipcMain, shell } from 'electron';
 import log from 'electron-log/main';
